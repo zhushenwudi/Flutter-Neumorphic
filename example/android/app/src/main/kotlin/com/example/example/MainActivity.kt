aba0e8fb8977bbe82cbfddadc91482f619ff56a3
zhushenwudi <55681140@163.com>
2024-08-29 22:13:53 +0800

    first commit

---
+++ example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    val myString = "adding this one cuz codefactor does not allow an empty function!"
+}
---
+++ example/lib/lib/back_button.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+
+class NeumorphicBack extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return NeumorphicButton(
+      padding: EdgeInsets.all(18),
+      style: NeumorphicStyle(
+        boxShape: NeumorphicBoxShape.circle(),
+        shape: NeumorphicShape.flat,
+      ),
+      child: Icon(
+        Icons.arrow_back,
+        color: NeumorphicTheme.isUsingDark(context)
+            ? Colors.white70
+            : Colors.black87,
+      ),
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
+    );
+  }
+}
